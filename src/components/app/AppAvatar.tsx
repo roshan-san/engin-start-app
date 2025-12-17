2cb406f0dc96e3d94ccc1f3cc4ae53d473939436
roshanjeyarubanr <roshanjeyarubanr@gmail.com>
2025-12-17 13:50:44 +0530

    UPDATE AVATAR STYLE

--- src/components/app/AppAvatar.tsx
+++ src/components/app/AppAvatar.tsx
@@ -2,7 +2,6 @@ import { Link, useRouteContext, useRouter } from "@tanstack/react-router";
 import { HelpCircle, LogOut, Settings } from "lucide-react";
 import { toast } from "sonner";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
-import { Button } from "~/components/ui/button";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
@@ -19,59 +18,64 @@ export function AppAvatar() {
 		from: "/(authenticated)/a",
 	});
 	const router = useRouter();
+	const fullName =
+		profile.full_name.charAt(0).toUpperCase() + profile.full_name.slice(1);
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button className="relative h-10 w-10 rounded-full" variant="ghost">
-					<Avatar>
-						<AvatarImage
-							alt={profile.full_name}
-							src={profile.avatar_url || undefined}
-						/>
-						<AvatarFallback>{profile.full_name.charAt(0)}</AvatarFallback>
-					</Avatar>
-				</Button>
+				<Avatar className="rounded-xl">
+					<AvatarImage
+						alt={profile.full_name}
+						src={profile.avatar_url || undefined}
+					/>
+					<AvatarFallback>
+						{profile.full_name.charAt(0).toUpperCase()}
+					</AvatarFallback>
+				</Avatar>
 			</DropdownMenuTrigger>
-			<DropdownMenuContent align="end" className="py-2">
+			<DropdownMenuContent align="end" className="p-2 w-auto border-0">
 				<DropdownMenuLabel>
-					<div className="flex flex-col space-y-1">
-						<p className="text-sm">{profile.full_name}</p>
+					<div className="flex flex-col gap-2">
+						<p className="text-sm">{`Hey ${fullName} !`}</p>
 						<p className="text-muted-foreground text-xs">{profile.email}</p>
 					</div>
 				</DropdownMenuLabel>
 				<DropdownMenuSeparator />
-				<DropdownMenuItem>
-					<Link
-						className="flex w-full items-center gap-2"
-						to="/a/settings/profile"
-					>
+				<DropdownMenuItem asChild>
+					<Link className="flex items-center gap-2" to="/a/settings/profile">
 						<Settings />
-						Settings
+						<p>Settings</p>
 					</Link>
 				</DropdownMenuItem>
 				<DropdownMenuItem>
-					<HelpCircle />
-					Help
+					<Link to="/a/dashboard" className="flex items-center gap-2">
+						<HelpCircle />
+						<p>Help</p>
+					</Link>
 				</DropdownMenuItem>
 				<DropdownMenuSeparator />
 				<DropdownMenuItem
+					variant="destructive"
+					className="flex items-center gap-2"
 					onClick={async () => {
-						toast.warning("Signing out...");
 						await authClient.signOut({
 							fetchOptions: {
 								onResponse: async () => {
-									// manually set to null to avoid unnecessary refetching
 									queryClient.setQueryData(authQueryOptions().queryKey, null);
 									await router.invalidate();
+								},
+								onSuccess: async () => {
+									toast.warning("Signing out...");
+								},
+								onError: async () => {
+									toast.error("Failed to Sign Out");
 								},
 							},
 						});
 					}}
-					className="w-full"
-					variant="destructive"
 				>
 					<LogOut />
-					Logout
+					<p>Logout</p>
 				</DropdownMenuItem>
 			</DropdownMenuContent>
 		</DropdownMenu>
