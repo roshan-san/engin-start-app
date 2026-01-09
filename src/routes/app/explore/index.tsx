ce7caeca351791d8c2755de6ca3f2a29f5092b7d
Roshan TSX <roshantsx@email.com>
2026-01-09 19:08:34 +0530

    check startup stuff

--- src/routes/app/explore/index.tsx
+++ src/routes/app/explore/index.tsx
@@ -1,13 +1,40 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Icon, SearchIcon } from "lucide-react";
+import { useAppForm } from "~/components/form/AppForm";
 
 export const Route = createFileRoute("/app/explore/")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
+	const form = useAppForm({});
 	return (
-		<div className="bg-card rounded-xl flex-1 flex items-center justify-center p-2 gap-2">
-			<div className="text-center">Explore</div>
+		<div className="flex-col flex-1 flex items-center justify-center gap-2">
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					form.handleSubmit();
+				}}
+			>
+				<form.AppField name="search">
+					{(field) => {
+						return (
+							<>
+								<field.TextInput
+									id="search"
+									label=""
+									placeholder="Explore Startups ...."
+								/>
+								<SearchIcon />
+							</>
+						);
+					}}
+				</form.AppField>
+				<div className="bg-card flex justify-end w-full rounded-xl p-2"></div>
+				<div className="w-100 h-100 bg-card rounded-xl justify-center flex items-center">
+					<p>Explore</p>
+				</div>
+			</form>
 		</div>
 	);
 }
